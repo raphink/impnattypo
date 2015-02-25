38f2db00c522178bcc6f9fbdc62a32fd27a393e7
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-02-25 15:47:42 +0100

    Fix specs with fixed indexes

--- spec/pdf_spec.rb
+++ spec/pdf_spec.rb
@@ -2,8 +2,8 @@ require 'pdf/reader'
 
 describe 'Impnattypo PDF' do
   reader = PDF::Reader.new('impnattypo-fr.pdf')
-  it 'should have 11 pages' do
-    reader.page_count.should eq(11)
+  it 'should have 13 pages' do
+    reader.page_count.should eq(13)
   end
   it 'should be made by TeX' do
     reader.info[:Creator].should eq('TeX')
@@ -11,8 +11,8 @@ describe 'Impnattypo PDF' do
   it 'should have page 1 with given media box' do
     reader.pages[0].attributes[:MediaBox].should eq([0, 0, 612, 792])
   end
-  it 'should have 4 fonts on page 2' do
-    reader.pages[1].fonts.keys.size.should eq(4)
+  it 'should have 5 fonts on page 2' do
+    reader.pages[1].fonts.keys.size.should eq(5)
   end
   it 'should start with a title' do
     reader.pages[0].text.should match('impnattypo')
